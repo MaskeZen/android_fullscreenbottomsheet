5554f837bdf201b046894af65e41770b72c8b1f6
Diego Mascheroni <maskeee@gmail.com>
2020-05-01 16:49:04 -0300

    ejemplo de bottomsheet

--- app/src/main/java/com/example/fullscreenbottomsheet/MainActivity.java
+++ app/src/main/java/com/example/fullscreenbottomsheet/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.fullscreenbottomsheet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.fullscreenbottomsheet.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        init();
     }
+
+    private void init() {
+        bi.openBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheet bottomSheet = new BottomSheet();
+                bottomSheet.show(getSupportFragmentManager(),bottomSheet.getTag());
+            }
+        });
+    }
+
 }
